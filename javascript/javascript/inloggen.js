d5e56eec655cf19fa8943fa60e1ef664127a4f38
4lexks <99203252+4lexks@users.noreply.github.com>
2024-05-17 18:21:03 +0200

    Delete javascript directory, want ik had een fout gemaakt. Ik zet het meteen weer goed

--- javascript/javascript/inloggen.js
+++
@@ -1,9 +0,0 @@
-// Voor wachtwoord laten zien bij klikken van de checkbox
-function WachtwoordZien() {
-  var x = document.getElementById("NepWachtwoord");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
-}
